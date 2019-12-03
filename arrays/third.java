792979f97a803dcfa44a59155aa7cd8df9df3e97
adamcsigas <csigasadam@gmail.com>
2019-12-03 11:46:48 +0100

    structural changes

--- arrays/third.java
+++ arrays/third.java
@@ -1,3 +1,5 @@
+package arrays;
+
 public class third {
     public static void main(String[] args) {
         int[] q = {4,5,6,7};
@@ -5,5 +7,5 @@ public class third {
     }
     // - Create an array variable named `q`
     //   with the following content: `[4, 5, 6, 7]`
-    // - Print the third element of `q`
+    // - Print the arrays.third element of `q`
 }
